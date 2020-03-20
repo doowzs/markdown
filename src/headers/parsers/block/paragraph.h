c4360d77eade872c6f342b4351b785c84de6c617
Tianyun Zhang <doowzs@qq.com>
2020-03-21 01:12:44 +0800

    fix wrong length in block paragraph parser

--- src/headers/parsers/block/paragraph.h
+++ src/headers/parsers/block/paragraph.h
@@ -14,13 +14,8 @@ public:
     this->master = master;
   }
   size_t parseBlock(DOM::Node *parent, const char *input, const size_t size) override {
-    size_t length = 0;
-    string content = string();
-    while (*input != '\n') {
-      content += *input;
-      ++input;
-      ++length;
-    }
+    size_t length = get_line_length(input);
+    string content = string(input, length);
     auto *paragraph = new DOM::Node(DOM::P);
     master->parseInline(paragraph, content.c_str(), length);
     parent->addChild(paragraph);
